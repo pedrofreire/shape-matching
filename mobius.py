bec3de5a4df05ead9aaf5e5207d1fe57eb59576b
Pedro Freire <pedrofreirex@gmail.com>
2018-12-07 15:24:45 +0100

    Fix embedding and refactor

--- mobius.py
+++ mobius.py
@@ -13,16 +13,18 @@ class Mesh:
         if not faces: faces = []
         self.vertices = vertices.copy()
         self.faces = faces.copy()
-        self._calculate_neighbors()
-        self._calculate_edge_mesh()
 
-    def _calculate_neighbors(self):
-        self.neighbors = [[] for _ in self.vertices]
-        for face in self.faces:
-            for u in face:
-                for v in face:
-                    if u != v:
-                        self.neighbors[u].append(v)
+        self.u = None
+        self.ul = None
+        self.me_vertices = None
+        self.me_faces = None
+        self.me_vertex_to_edge = None
+        self.me_neighbors = None
+        self.edge_to_me_vertex = None
+        self.cut_face = None
+        self.embedding = None
+
+        self._calculate_edge_mesh()
 
     def _calculate_edge_mesh(self):
         self.me_vertices = []
@@ -30,8 +32,7 @@ class Mesh:
 
         self.edge_to_me_vertex = {}
 
-        # Initialize bigger list, prune excess later.
-        self.me_neighbors = [[] for _ in range(3*len(self.faces))]
+        self.me_neighbors = []
 
         for face in self.faces:
             me_face = []
@@ -39,19 +40,18 @@ class Mesh:
                 if edge not in self.edge_to_me_vertex:
                     self.edge_to_me_vertex[edge] = len(self.edge_to_me_vertex)
                     self.me_vertices.append(self.get_mid_edge_point(edge))
+                    self.me_neighbors.append([])
                 me_face.append(self.edge_to_me_vertex[edge])
 
             self.me_faces.append(me_face)
-            for u in me_face:
-                for v in me_face:
-                    if u != v:
-                        self.me_neighbors[u].append(v)
+            for u, v in zip(me_face, me_face[1:] + [me_face[0]]):
+                self.me_neighbors[u].append((v, 1))
+                self.me_neighbors[v].append((u, -1))
 
         self.me_vertex_to_edge = {}
         for key, value in self.edge_to_me_vertex.items():
             self.me_vertex_to_edge[value] = key
 
-        self.me_neighbors = self.me_neighbors[:len(self.me_vertices)]
 
     def get_mid_edge_point(self, edge):
         u, v = edge
@@ -76,13 +76,24 @@ class Mesh:
         self.display_embedding()
 
     def display_embedding(self):
+        LENGTH_THRESHOLD = 30000
         fig, ax = plt.subplots()
-        for me_face in self.me_faces[:1000]:
-            if self.is_cut_me_face(me_face):
+        for me_face in self.me_faces[:]:
+            if self.is_cut_me_face(me_face) or self.embedded_triangle_length(me_face) > LENGTH_THRESHOLD:
                 continue
             x, y = zip(*(self.embedding[vr] for vr in me_face+[me_face[0]]))
             ax.plot(x, y, '-')
         plt.show()
+
+    def embedded_triangle_length(self, me_face):
+        triangle = list(self.embedding[vr] for vr in me_face)
+        length = 0
+        for (px, py), (qx, qy) in zip(triangle, triangle[1:] + triangle[:1]):
+            x = px - qx
+            y = py - qy
+            length += np.sqrt(x**2 + y**2)
+        return length
+
 
     def is_cut_me_face(self, me_face):
         return sorted(me_face) == sorted(self.edge_to_me_vertex[edge] for edge in self.get_face_edges(self.cut_face))
@@ -90,86 +101,62 @@ class Mesh:
     def calculate_u(self):
         """ Calculate u (harmonic function on vertices) by solving
             sparse linear system. """
-        A = defaultdict(float)
-        b = defaultdict(float)
+        n = len(self.vertices)
+        b = np.zeros(n-2)
+        A_dict = defaultdict(float)
 
-        # The embedding should be quasi-invariant mod Mobius transformation,
-        # so selecting the cut-face smartly is not very important.
-        self.cut_face = self.faces[0]
 
-        # We implicitly set u[u1] = 1 and u[um1] = -1
-        um1, u1, _ = sorted(self.cut_face)
-        print(um1, u1)
+        # TODO: implement better cut_face choice algorithm.
+        self.cut_face = self.faces[3000]
+        x, y, z = self.cut_face
+
+        # We will set u[cm1] = -1 and u[c1] = 1.
+        # Without loss of generality, we select cm1 as smaller than c1.
+        if x < y:
+            cm1, c1 = x, y
+        elif y < z:
+            cm1, c1 = y, z
+        else:
+            cm1, c1 = z, x
+
 
         for face in self.faces:
             for (u, v), angle in zip(self.get_face_edges(face), self.get_face_angles(face)):
                 cot = 1 / np.tan(angle)
-                for othr, vtx in ((u, v), (v, u)):
-                    A[(vtx, vtx)] += cot
-                    A[(othr, vtx)] -= cot
+                A_dict[(u, u)] += cot
+                A_dict[(v, u)] -= cot
+                A_dict[(u, v)] -= cot
+                A_dict[(v, v)] += cot
 
-
-        n = len(self.vertices)
         rows = []
         cols = []
         data = []
-        # um1 = u1 = 100000
-        for (i, j), val in A.items():
-            # print((i, j), val)
-            if i in (um1, u1):
+        # We remove rows cm1 and c1, and we
+        # move columns cm1 and c1 to b[].
+        for (i, j), val in A_dict.items():
+            if i in (cm1, c1):
                 continue
-            i -= (i >= um1) + (i >= u1)
-            if j == um1:
+            i -= (i >= cm1) + (i >= c1)
+            if j == cm1:
                 b[i] += val
-            elif j == u1:
+            elif j == c1:
                 b[i] -= val
             else:
-                j -= (j >= um1) + (j >= u1)
+                j -= (j >= cm1) + (j >= c1)
                 rows.append(i)
                 cols.append(j)
                 data.append(val)
 
-        """
-        Ans = np.zeros((n, n))
-        for (i, j), val in A.items():
-            Ans[i, j] = val
-        print(Ans)
-        """
         A = csc_matrix((data, (rows, cols)), shape=(n-2, n-2))
-
-        rows = []
-        cols = []
-        data = []
-        bl = np.zeros(n-2)
-        for j, val in b.items():
-            bl[j] = val
-            rows.append(j)
-            cols.append(0)
-            data.append(val)
-        b = bl
-
-        print('A')
-        print(A.toarray())
-        print('b')
-        print(b)
         u = spsolve(A, b)
-        print(u)
-        # u[um1] = -1
-        # u[u1] = 1
-        # print(lsqr_data)
+
+        # Insert values of cm1 and c1 into to the solution vector.
         self.u = np.zeros(n)
-        self.u[:um1] = u[:um1]
-        self.u[um1] = -1
-        self.u[um1+1:u1] = u[um1:u1-1]
-        self.u[u1] = 1
-        self.u[u1+1:] = u[u1-1:]
-        # The intuition is that those two values would
-        # be somewhat equal.
-        print(self.u)
-        print(len(self.u[self.u > 0]))
-        print(len(self.u[self.u < 0]))
-        print('----------')
-        print(np.sum(abs(A @ u - b)))
+        self.u[:cm1] = u[:cm1]
+        self.u[cm1] = -1
+        self.u[cm1+1:c1] = u[cm1:c1-1]
+        self.u[c1] = 1
+        self.u[c1+1:] = u[c1-1:]
 
     def calculate_ul(self):
         """ Calculate ul (harmonic function on mid-edge vertices) 
@@ -186,19 +173,14 @@ class Mesh:
 
         while frontier:
             node = frontier.pop()
-            for neighbor in self.me_neighbors[node]:
+            for neighbor, orientation in self.me_neighbors[node]:
                 if neighbor not in visited:
-                    self.calculate_one_ul(node, neighbor)
+                    self.calculate_one_ul(node, neighbor, orientation)
                     visited.add(neighbor)
                     frontier.appendleft(neighbor)
 
-        print(self.ul)
-        print(len(self.ul[self.ul > 0]))
-        print(len(self.ul[self.ul < 0]))
 
-
-
-    def calculate_one_ul(self, vs, vr):
+    def calculate_one_ul(self, vs, vr, orientation):
         er = self.me_vertex_to_edge[vr]
         es = self.me_vertex_to_edge[vs]
         vj = next(iter((set(er) & set(es))))
@@ -209,28 +191,31 @@ class Mesh:
         ui = self.u[vi]
         uj = self.u[vj]
         uk = self.u[vk]
-        diffs = abs(ui - uj), abs(ui - uk), abs(uj - uk)
-        # if max(np.log(diffs)) > -2:
-            # print(*('{:.2f}'.format(diff) for diff in diffs))
 
         diff = (((ui - uj) / np.tan(ang_k))  +  ((uk - uj) / np.tan(ang_i))) / 2
-        self.ul[vr] = self.ul[vs] + diff
+        self.ul[vr] = self.ul[vs] + orientation * diff
 
 
     def get_face_edges(self, face): 
-        """ Get ordered opposite edges. """
-        u, v, w = sorted(face)
-        return [(v, w), (u, w), (u, v)]
+        """ Get edges in order. Note that the convention used for an edge here
+            is that the first element is always smaller than the second one. """
+        edges = []
+        for u, v in zip(face, face[1:] + face[:1]):
+            edge = (u, v) if u < v else (v, u)
+            edges.append(edge)
+        return edges
 
     def get_face_angles(self, face):
-        """ Get ordered angles. """
+        """ Get opposite angles to edges, in order. """
         angles = []
-        dface = sorted(face) + sorted(face)
-        for i in range(len(face)): 
-            angles.append(self.get_angle(*dface[i:i+3]))
+        u, v, w = face
+        angles.append(self.get_angle(w, u, v))
+        angles.append(self.get_angle(u, w, v))
+        angles.append(self.get_angle(v, w, u))
         return angles
 
     def get_angle(self, u, v, w):
+        """ Returns angle between vectors uv and uw. """
         norm = lambda p : np.sqrt(sum(x**2 for x in p))
         p = self.vertices[u]
         q = self.vertices[v]
@@ -257,8 +242,9 @@ def read_mesh(filename):
 
 
 def main():
-    mesh = read_mesh('./datasets/non-rigid-world/cat1.obj')
+    mesh = read_mesh('./datasets/non-rigid-world/cat0.obj')
     # mesh = read_mesh('./datasets/simple/tetra.obj')
+    # mesh = read_mesh('./datasets/simple/reg_tetra.obj')
     mesh.calculate_planar_embedding()
 
 if __name__ == '__main__':
--- view_mesh.js
+++ view_mesh.js
@@ -3,7 +3,8 @@
 function start() {
   const scenewrapper = new SceneWrapper();
   const loader = new THREE.OBJLoader();
-  loader.load('./datasets/non-rigid-world/cat1.obj', (obj) => { 
+  loader.load('./datasets/non-rigid-world/victoria0.obj', (obj) => { 
+  // loader.load('./datasets/simple/reg_tetra.obj', (obj) => { 
     scenewrapper.scene.add(obj); 
   });
 }
